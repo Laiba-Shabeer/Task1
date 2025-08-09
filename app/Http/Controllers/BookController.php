aa3358841e93c0fa04555adf4fb741ce22157215
Laiba Shabeer <152430296+Laiba-Shabeer@users.noreply.github.com>
2025-08-09 21:23:36 +0500

    first commit

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+  use App\Models\Book;
+use Illuminate\Http\Request;
+
+
+class BookController extends Controller
+{
+
+
+public function index()
+{
+    return Book::all();
+}
+
+public function store(Request $request)
+{
+    $request->validate([
+        'title' => 'required',
+        'author' => 'required',
+        'price' => 'required|numeric',
+        'isbn' => 'required|unique:books',
+        'published_date' => 'required|date'
+    ]);
+
+    return Book::create($request->all());
+}
+
+public function show($id)
+{
+    return Book::findOrFail($id);
+}
+
+public function update(Request $request, $id)
+{
+    $book = Book::findOrFail($id);
+    $book->update($request->all());
+    return $book;
+}
+
+public function destroy($id)
+{
+    Book::destroy($id);
+    return response()->json(['message' => 'Book deleted']);
+}
+
+}
